258d21be1d76ebcdade303acd245c61bcb1e666a
Daniel <dcmckay10@gmail.com>
2020-04-03 10:31:37 -0600

    build and deploy to firebase

---
+++ build/precache-manifest.15b3bb59fda82514abefcab34904f357.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468c6291abbed5044a143660fa2d107b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36f13a7a6ba60a19a5ff",
+    "url": "/static/css/main.ae2c2932.chunk.css"
+  },
+  {
+    "revision": "2eb92e863ca55dc8601c",
+    "url": "/static/js/2.9687f5eb.chunk.js"
+  },
+  {
+    "revision": "f3af4082548dce8552c2f66d7bc1b6c6",
+    "url": "/static/js/2.9687f5eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f13a7a6ba60a19a5ff",
+    "url": "/static/js/main.83c0875b.chunk.js"
+  },
+  {
+    "revision": "77f5bf0e149ef3f8761a",
+    "url": "/static/js/runtime-main.bf8de95f.js"
+  },
+  {
+    "revision": "17db876e3ce55322f7805a196576930b",
+    "url": "/static/media/buddhabg.17db876e.jpg"
+  },
+  {
+    "revision": "b066ba0ce4e2cbb3662340b5ddc0441d",
+    "url": "/static/media/loginbg.b066ba0c.jpg"
+  }
+]);
